182f5bd0da29328b82783cac9a220191465d5dbf
Alexander Pankratov <a.pankratov@astarus.com>
2022-10-07 12:43:18 +0400

    save

--- lib/provider/event_bus_provider.dart
+++ lib/provider/event_bus_provider.dart
@@ -10,23 +10,29 @@ abstract class EventProvider implements ChangeNotifier, EventBus {
 }
 
 class BasicEventProvider with ChangeNotifier implements EventProvider {
+  @override
   String? get prefix => _controller.prefix;
-  EventBus _controller;
+  final EventBus _controller;
   BasicEventProvider(this._controller);
+  @override
   Type get type => runtimeType;
+
   @override
   bool contain<T>(String? eventName) {
     return _controller.contain<T>(eventName);
   }
 
+  @override
   T? lastEvent<T>({String? eventName, String? prefix}) {
     return _controller.lastEvent<T>(eventName: eventName, prefix: prefix);
   }
 
+  @override
   bool send<T>(T event, {String? eventName, String? uuid, String? prefix}) {
     return _controller.send<T>(event, eventName: eventName, uuid: uuid, prefix: prefix);
   }
 
+  @override
   Stream<EventDTO<T>>? listenEventDTO<T>({String? eventName, bool repeatLastEvent = false, String? prefix}) {
     bool needNotify = !_controller.contain<T>(eventName);
     var ret = _controller.listenEventDTO<T>(eventName: eventName, repeatLastEvent: repeatLastEvent, prefix: prefix);
@@ -38,6 +44,7 @@ class BasicEventProvider with ChangeNotifier implements EventProvider {
     return ret;
   }
 
+  @override
   Stream<T>? listenEvent<T>({String? eventName, bool repeatLastEvent = false, String? prefix}) {
     bool needNotify = !_controller.contain<T>(eventName);
     var ret = _controller.listenEvent<T>(eventName: eventName, repeatLastEvent: repeatLastEvent, prefix: prefix);
@@ -49,6 +56,7 @@ class BasicEventProvider with ChangeNotifier implements EventProvider {
     return ret;
   }
 
+  @override
   bool repeat<T>({String? eventName, String? uuid, String? prefix}) {
     return _controller.repeat(eventName: eventName, uuid: uuid, prefix: prefix);
   }
